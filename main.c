95335ed877452e6cc62740b2d4bff64a0023b119
Abdulrahman Ismael <116180351+AbdulrahmanIsmael@users.noreply.github.com>
2025-08-01 19:32:03 +0300

    refactor

--- main.c
+++ main.c
@@ -18,24 +18,21 @@ int main()
     {
     case '+':
         result = num1 + num2;
-        printf("Result: %.2lf + %.2lf = %.2lf\n", num1, num2, result);
         break;
     case '-':
         result = num1 - num2;
-        printf("Result: %.2lf - %.2lf = %.2lf\n", num1, num2, result);
         break;
     case '*':
         result = num1 * num2;
-        printf("Result: %.2lf * %.2lf = %.2lf\n", num1, num2, result);
         break;
     case '/':
         result = num1 - num2;
-        printf("Result: %.2lf / %.2lf = %.2lf\n", num1, num2, result);
         break;
     default:
         printf("Error: Invalid operator (%c). Please use +, -, *, or /.\n", operator);
         main();
     }
+    printf("Result: %.2lf %c %.2lf = %.2lf\n", num1, operator, num2, result);
 
     printf("\n");
     printf("Thank you for using the Simple Calculator Program!\n");
